b4583c32879a30f8d4dbf63b5e09fcf700ef87f5
Jeril-Albi <devjerilalbi@gmail.com>
2021-12-07 16:59:14 +0530

    menu bar open n close

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-//navigation bar
-const hambeger = document.querySelector('.hamberger')
-const navlist = document.querySelector('.nav-list')
 
-hambeger.addEventListener('click',()=>{
-    navlist.classList.toggle("open");
-})
+const navMenu = document.getElementById('nav_menu')
+const navToggle = document.getElementById('nav_toggle')
+const navClose = document.getElementById('nav_close')
+
+//show menu
+if(navToggle){
+    navToggle.addEventListener('click',() => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//close menu
+if(navClose){
+    navClose.addEventListener('click',() => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+//remove menu icon
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
