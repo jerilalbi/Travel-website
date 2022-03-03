ba39833330f3a10155ec4287077ca01eb3389e5a
Jeril-Albi <devjerilalbi@gmail.com>
2022-03-03 22:43:08 +0530

    weather added

--- assets/js/place_view.js
+++ assets/js/place_view.js
@@ -46,7 +46,7 @@ function initMap() {
         }));
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
-    
+
         localStorage.setItem('place_lat',place.geometry.location.lat());
         localStorage.setItem('place_long',place.geometry.location.lng());
 
@@ -64,6 +64,10 @@ function initMap() {
     });
   }
 
+  var place_lat = Number(localStorage.getItem('place_lat'));
+  var place_lng = Number(localStorage.getItem('place_long'));
+  
+
   //API Fetch
   function place_attraction(lat,lng){
     fetch(`https://travel-advisor.p.rapidapi.com/attractions/list-by-latlng?longitude=${lng}&latitude=${lat}&lunit=km&currency=USD&lang=en_US`, {
@@ -76,8 +80,8 @@ function initMap() {
     .then(response => {
         return response.json();
     }).then((data) => {
+        var place_address = `${data.data[0].address_obj.city}, ${data.data[0].address_obj.country}`
         var side_data = '';
-        console.log(data.data);
         data['data'].map((value,index) => {
             if(data.data[index].ad_position == undefined){
                     side_data += `
@@ -92,16 +96,94 @@ function initMap() {
             }
         });
         document.getElementById('show_data').innerHTML = side_data;
+        document.getElementById('show_place_address').innerHTML = place_address;
     })
     .catch(err => {
         console.error(err);
     });
   }
-  
-  place_attraction(Number(localStorage.getItem('place_lat')),Number(localStorage.getItem('place_long')));
+
+  place_attraction(place_lat,place_lng);
+
+  function food_attraction(lat,lng){
+    fetch(`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?longitude=${lng}&latitude=${lat}&lunit=km&currency=USD&lang=en_US`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+            "x-rapidapi-key": "d8bb2d3d02msh15482b0d942a663p1751e6jsnf05b2c79ff7a"
+        }
+    })
+    .then(response => {
+        return response.json();
+    }).then((data) => {
+        var side_data = '';
+        data['data'].map((value,index) => {
+            if(data.data[index].ad_position == undefined){
+                    side_data += `
+            <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">${data.data[index].name}</h5>
+                          <p class="card-text">${data.data[index].description}</p>
+                          <p class="card-text" style="color: green; font-weight: bold;">Price: ${data.data[index].price}</p>
+                          <a href="${data.data[index].web_url}" class="btn btn-primary">View More</a>
+                        </div>
+            </div>
+            `
+            }
+        });
+        document.getElementById('show_data').innerHTML = side_data;
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  }
+
+  function weatherFetch(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=5e8ad7b6f198bc4eb1edd093db935a60`)
+    .then(response => {
+        return response.json();
+    }).then((data) => {
+        var weather_data_full = data.main.temp - 273.15;
+        var weather_data = Math.round(weather_data_full * 10) / 10;
+        var weather_img = ` http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById('weather_img').src = weather_img;
+        document.getElementById('weather_data').innerHTML = `${weather_data} &#176; C`;
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  }
+
+  weatherFetch(place_lat,place_lng);
+
+  document.getElementById('type_select').onchange = function(){
+      var value = document.getElementById('type_select').value;
+        
+      switch(value){
+          case 'attraction' : place_attraction(place_lat,place_lng);
+                              break;
+          case 'restaurants' : food_attraction(place_lat,place_lng);
+                              break;
+      }
+  }
 
   var submit_btn = document.getElementById('search_btn');
-  submit_btn.onclick = function(){  
-      console.log(Number(localStorage.getItem('place_lat')))
-      place_attraction(Number(localStorage.getItem('place_lat')),Number(localStorage.getItem('place_long')));
+  submit_btn.onclick = function(){
+
+    var new_place_lat = Number(localStorage.getItem('place_lat'));
+    var new_place_lng = Number(localStorage.getItem('place_long'));
+
+    weatherFetch(new_place_lat,new_place_lng);
+
+    document.getElementById('type_select').selectedIndex = 0;
+    document.getElementById('type_select').value = 'attraction';
+
+        var value = document.getElementById('type_select').value;
+  
+        switch(value){
+            case 'attraction' : place_attraction(new_place_lat,new_place_lng);
+                                break;
+            case 'restaurants' : food_attraction(new_place_lat,new_place_lng);
+                                break;
+        }
   }
--- assets/js/tour_package.js
+++ assets/js/tour_package.js
@@ -1,18 +1,44 @@
 function changeData(){
 
+  var nameCheck = ['london','maldives','dubai','berlin','paris','liverpool','india','denver','norway','tokyo','new york','stockholm','venice','rio','amsterdam','nairobi',]
+
     let show_data = '';
+    var check = false;
     var tour_package_data = JSON.parse(localStorage.getItem('tour_package_data'));
 
-    tour_package_data[Number(localStorage.getItem('package_index'))]['package'].map((value) => {
-        show_data += `
+    for(var i = 0; i<tour_package_data.length; i++){
+      if(tour_package_data[Number(localStorage.getItem('package_index'))].name.toLowerCase() == nameCheck[i]){
+        check = true;
+      }
+    }  
+
+    tour_package_data[Number(localStorage.getItem('package_index'))]["package"].map((value) => {
+      
+      const duration = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+      const price = Math.floor(Math.random() * (75 - 60 + 1)) + 60;
+
+        if(check){
+          show_data += `
         <div class="card" style="width: 28rem;">
                         <div class="card-body">
-                          <h5 class="card-title">${value['duration']} <img src="${value['part_logo']}" style="width: 4rem; height: 2rem; float: right;" alt="" srcset=""></h5>
+                          <h5 class="card-title">${duration} Nights ${duration + 1} Days <img src="${value['part_logo']}" style="width: 4rem; height: 2rem; float: right;" alt="" srcset=""></h5>
                           <p class="card-text">${value['places']}</p>
-                          <span style="font-size: 25px; font-weight: bold; color: green;">${value['price']}</span><a href="login_customer.html" class="btn btn-primary" style="float: right;">Book Now</a>                
+                          <span style="font-size: 25px; font-weight: bold; color: green;">$ ${price}</span><a href="login_customer.html" class="btn btn-primary" style="float: right;">Book Now</a>                
                         </div>
         </div>
         `
+        } else{
+          show_data += `
+        <div class="card" style="width: 28rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">${value.duration}<img src="${value['part_logo']}" style="width: 4rem; height: 2rem; float: right;" alt="" srcset=""></h5>
+                          <p class="card-text">${value['places']}</p>
+                          <span style="font-size: 25px; font-weight: bold; color: green;">${value.price}</span><a href="login_customer.html" class="btn btn-primary" style="float: right;">Book Now</a>                
+                        </div>
+        </div>
+        `
+        }
+        
     });
 
     document.getElementById('pack_data').innerHTML = show_data;
